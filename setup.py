3d521def889586fc04f1fdcf70556bf2c2ebcb3f
Andreas Schliebitz <a.schliebitz@hs-osnabrueck.de>
2023-07-14 23:21:32 +0200

    Pinning some requirements and creating example.py.

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@
 
 from setuptools import setup, find_packages
 
-# https://stackoverflow.com/questions/28509965/setuptools-development-requirements
 setup(
-    name="Hyperspectral Image Preprocessing",
+    name="hipp",
     version="0.1",
     extras_require={"dev": ["black"]},
     packages=find_packages(),
